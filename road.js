757eec08bce39345b8ec8d7520eb7cf316f63c05
flyingarburator <76648785+flyingarburator@users.noreply.github.com>
2022-05-19 21:13:10 +0300

    Riad more details

--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road{
-    constructor(x, width, laneCount=3){
+    constructor(x, width, laneCount=4){
         this.x = x;
         this.width = width;
         this.laneCount = laneCount;
@@ -17,18 +17,23 @@ class Road{
 
 
         for (let i =0;i<=this.laneCount;i++){
-            
+            const x=lerp(
+                this.left,
+                this.right,
+                i/this.laneCount,
+            );
+            if(i>0 && i<this.laneCount){
+                clearTimeout.setLineDash([20,20]);
+            }
+            else{
+                ctx.setLineDash([]);
+            }
+
+            ctx.beginPath();
+            ctx.moveTo(x,this.top);
+            ctx.lineTo(x,this.bottom);
+            ctx.stroke();
         }
-
-        ctx.beginPath();
-        ctx.moveTo(this.left,this.top);
-        ctx.lineTo(this.left,this.bottom);
-        ctx.stroke();
-
-
-        ctx.beginPath();
-        ctx.moveTo(this.right,this.top);
-        ctx.lineTo(this.right,this.bottom);
-        ctx.stroke();
     }
 }
+
